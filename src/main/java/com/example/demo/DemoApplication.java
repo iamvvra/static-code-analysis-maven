6d85bb677e8accb52818885847184aa6c4673dcd
Vijay Veeraraghavan <vijay.raghavan08@gmail.com>
2018-12-18 12:20:03 +0530

    Add code formater with pre-commit hook

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Splits a string on whitespace. */
 @SpringBootApplication
 public class DemoApplication {
+
+  /**
+   * Splits a string on whitespace.
+   *
+   * @param args The string to split. An {@code null} string is treated as an empty string.
+   */
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
   }
--- src/main/java/com/example/demo/resources/Hello.java
+++ src/main/java/com/example/demo/resources/Hello.java
@@ -1,15 +1,23 @@
 package com.example.demo.resources;
 
 import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+/** A volatile storage for objects based on a key, which may be invalidated and discarded. */
 @RestController
 public class Hello {
 
+  /**
+   * Says hello to the name
+   *
+   * @param name The string to split. An {@code null} string is treated as an empty string.
+   * @return A hello + the name
+   */
   @GetMapping("/hello/{name}")
-  public String sayHello(@PathVariable String name) {
+  public String sayHello(@PathVariable final String name) {
     return "Hello " + Optional.ofNullable(name).orElse("world");
   }
 }
